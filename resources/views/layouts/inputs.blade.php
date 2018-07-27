7a1f5e9dfa7c41b9b565f1c57082be1e2e3c332f
Ryan Borja <ryan.t.bortja@gmail.com>
2018-07-27 16:45:15 -0400

    added a vue component and changed the home page

---
+++ resources/views/layouts/inputs.blade.php
@@ -0,0 +1,8 @@
+<div class="input-group mb-3">
+    <input class="form-control" id="searchInput" type="text" placeholder="Search by name..">
+    <div class="input-group-append">
+        <button class="btn btn-primary" type="button">Create new contact</button> 
+        {{-- <button class="btn btn-warning" type="button">Import</button> --}}
+        <button class="btn btn-success" type="button">Export your contacts</button>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Unicorn
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div>
+                    <h2>an app to store and share your contacts</h2>
                 </div>
             </div>
         </div>
